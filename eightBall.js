96cb768fbc1116bab3793387032888d8a3a7a1da
Philip Owusu Asare <philip.asare@amalitech.org>
2021-07-25 10:36:48 +0000

    Magi Eight Ball project codecademy

---
+++ eightBall.js
@@ -0,0 +1,43 @@
+let userName = ''
+if (userName) {
+  console.log(`Hello ${userName}!`)
+}
+else {
+  console.log(`Hello!`)
+}
+
+let userQuestion = 'Will Ghana ever be a developed country?'
+console.log(`The user asked: ${userQuestion}`)
+
+let randomNumber = Math.floor(Math.random() * 8)
+let eightBall = ''
+
+switch (randomNumber) {
+  case 0: 
+  eightBall = 'It is certain'
+  break;
+  case 1: 
+  eightBall = 'It is decidedly so'
+  break;
+  case 2: 
+  eightBall = 'Reply hazy try again'
+  break;
+  case 3: 
+  eightBall = 'cannot predict now'
+  break;
+  case 4: 
+  eightBall = 'Do not count on it'
+  break;
+  case 5: 
+  eightBall = 'My sources say no'
+  break;
+  case 6: 
+  eightBall = 'Outlook not so good'
+  break;
+  case 7: 
+  eightBall = 'Signs point to yes'
+  break;
+  default: eightBall = 'Error'
+  break;
+}
+console.log(eightBall)
